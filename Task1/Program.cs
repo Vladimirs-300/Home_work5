f11a6ffab84826eb1543aa8386bca165fea666d1
vladimir <slr-klesks-300@yandex.ru>
2022-09-24 01:59:45 +1000

    домашка

---
+++ Task1/Program.cs
@@ -0,0 +1,49 @@
+﻿// Задача 1: Задайте массив заполненный случайными положительными трёхзначными числами.
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+
+// [345, 897, 568, 234] -> 2
+
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    bool isDigit = int.TryParse(Console.ReadLine(), out int number);
+    if (isDigit)
+    {
+        return number;
+    }
+    throw new Exception("Вы ввели не число");
+}
+
+int[] GenerateArray(int length, int minRandom, int maxRandom)
+{
+    Random rnd = new Random();
+    int[] answer = new int[length];
+    for (int i = 0; i < answer.Length; i++)
+    {
+        answer[i] = rnd.Next(minRandom, maxRandom + 1);
+    }
+    return answer;
+}
+
+void PrintArray(int[] array)
+{
+    int even = 0;
+    int uneven = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.Write(array[i] + "; ");
+        if (array[i] % 2 == 0) even++;
+        else uneven++;
+    }
+    System.Console.WriteLine();
+    System.Console.WriteLine($"Количество четных чисел {even}, количество нечетных чисел {uneven}");
+    System.Console.WriteLine();
+}
+
+const int MIN_ELEMENTS = 100;
+const int MAX_ELEMENTS = 999;
+
+int length = Prompt("Введите длину массива -> ");
+int[] array = GenerateArray(length, MIN_ELEMENTS, MAX_ELEMENTS);
+PrintArray(array);
---
+++ Task3/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 3: Задайте массив вещественных чисел. 
+// Найдите разницу между максимальным и минимальным элементов массива.
+
+// [3 7 22 2 78] -> 76
+
+int size = new Random().Next(1, 11);
+
+System.Console.WriteLine($"Количетсов элементов массива: {size} \t");
+
+double[] FillSourcearray(int size)
+{
+    double[] array = new double[size];
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random().NextDouble() * 100;
+    }
+    return array;
+}
+
+void PrintDiffMinMax(double[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.WriteLine(array[i]);
+    }
+    double difference = array.Max() - array.Min();
+    System.Console.WriteLine($"Разница между минимальным и максимальным значениями массива составляет {difference}");
+    System.Console.WriteLine();
+}
+
+PrintDiffMinMax(FillSourcearray(size));
+System.Console.WriteLine();
